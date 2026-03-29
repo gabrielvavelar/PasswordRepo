ad6661fea6beb0d332ee1815a734553050b74ee1
GabrielAvelarbr <99gabrielavelar@gmail.com>
2026-03-28 21:12:41 -0300

    Initial commit

---
+++ src/main/java/com/github/gabrielvavelar/PasswordRepo/PasswordRepoApplication.java
@@ -0,0 +1,13 @@
+package com.github.gabrielvavelar.PasswordRepo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PasswordRepoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PasswordRepoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/gabrielvavelar/PasswordRepo/PasswordRepoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.gabrielvavelar.PasswordRepo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PasswordRepoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
